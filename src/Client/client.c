210ad985e8f9304c89321a162400aac8d034d789
StevenGood154 <sgood2@nd.edu>
2022-02-02 21:29:09 -0500

    client now displays transfer speed. basic usage info added for server and client.

--- src/Client/client.c
+++ src/Client/client.c
@@ -1,27 +1,41 @@
 #include "client_socket.h"
+#include <sys/time.h>
 
 int main(int argc, char *argv[]) {
-	puts("Welcome client.");
+	puts("welcome client");
+	if (argc != 4) {
+		printf("usage: %s host port filename\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 	char *host = argv[1];
 	char *port = argv[2];
-	char *msg = strdup(argv[3]);
+	char *msg = argv[3];
+
+	// begin time
+	struct timeval start;
+	gettimeofday(&start, NULL);
 
 	// connect to server
 	int fd = socket_dial(host, port);
+	if (fd < 0) {
+		return EXIT_FAILURE;
+	}
+
+	// send length of filename
+	uint16_t name_length = strlen(msg);
+	char buffer[BUFSIZ];
+	sprintf(buffer, "%16hu", htons(name_length));
+	send(fd, buffer, 16, 0);
 
 	// send name of file
-	printf("fetching %s\n", msg);
-	size_t bytes = send(fd, msg, strlen(msg), 0);
-
-	// Parse incoming header
-	char buffer[BUFSIZ];
-	recv(fd, buffer, 5, 0);
-	buffer[5] = '\0';
-	if (strcmp("FOUND", buffer)) {
-		puts("ERROR: something went wrong"); // we cant tell the difference b/w the file not existing and the server crashing
+	size_t bytes = send(fd, msg, name_length, 0);
+	// recieve file size
+	recv(fd, buffer, 32, 0);
+	uint32_t file_length = ntohl(atol(buffer));
+	if (file_length == 0) {
+		puts("ERROR: something went wrong");
 		close(fd);
-		free(msg);
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}
 
 	FILE *fp = fopen(msg, "w");
@@ -32,9 +46,14 @@ int main(int argc, char *argv[]) {
 		byte_total += bytes;
 	} while (bytes > 0);
 
-	printf("SUCCESS: %zu bytes recieved\n", byte_total);
+	// end time
+	struct timeval end;
+	gettimeofday(&end, NULL);
 
-	free(msg);
+	// printf("SUCCESS: %zu bytes recieved\n", byte_total);
+	double delta = end.tv_sec - start.tv_sec + 0.000001 * (end.tv_usec - start.tv_usec);
+	printf("%zu bytes transferred over %lf seconds for a speed of %lf MB/sec.\n", byte_total, delta, byte_total/(1000000*delta));
+
 	close(fd);
 
 
--- src/Server/server.c
+++ src/Server/server.c
@@ -1,6 +1,10 @@
 #include "server_socket.h"
 
 int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("usage: %s port\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 	char *port = argv[1];
 	int server_fd = socket_listen(port);
 	if (server_fd < 0)
@@ -15,23 +19,32 @@ int main(int argc, char *argv[]) {
 
 		puts("accepted connection");
 
+		// recieve filename size
  		char buffer[BUFSIZ];
-		size_t bytes = recv(fd, buffer, BUFSIZ-1, 0);
-		buffer[bytes] = '\0';
+		recv(fd, buffer, 16, 0);
+		uint16_t name_length = ntohs(atoi(buffer));
+
+		// recieve filename
+		recv(fd, buffer, name_length, 0);
+		buffer[name_length] = '\0';
 
 		printf("fetching file: %s\n", buffer);
 
-		// send header
+		// send file size
 		FILE *fp = fopen(buffer, "r");
 		if (fp == NULL) {
 			fprintf(stderr, "ERROR: Requested file not found\n");
-			send(fd, "ERROR: Requested file not found", 31, 0);
+			sprintf(buffer, "%32u", 0);
+			send(fd, buffer, 32, 0);
 			continue;
 		}
-		send(fd, "FOUND", 5, 0);
+		fseek(fp, 0, SEEK_END);
+		sprintf(buffer, "%u\n", htonl(ftell(fp)));
+		send(fd, buffer, 32, 0);
 
-		// send file
-		size_t byte_total = 0;
+		// send file data
+		fseek(fp, 0, SEEK_SET);
+		size_t bytes, byte_total = 0;
 		do {
 			bytes = fread(buffer, 1, BUFSIZ, fp);
 			send(fd, buffer, bytes, 0);
